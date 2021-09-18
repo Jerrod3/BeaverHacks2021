a881e2749f5e852ba51e9eeed668d2e4a27c5c99
Jerrod Lepper <lepperj@oregonstate.edu>
2021-09-17 20:23:14 -0400

    "Added recycling function"

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ class User:
         self._num_vehicles = int(input("How many vehicles in your houehold? "))
         self._cars_list = []
         self._utilities_dict = {}
+        self._recycling_choices = []
+        self._footprint = 0
 
     def get_car_info(self):
         """
@@ -54,21 +56,68 @@ class User:
             "propane": propane,
         }
 
+    def get_recycling_info(self):
+
+        """Calculates recycling reductions"""
+
+        metal = input("Do you recycle metal cans?")
+
+        if metal in positive_responses():
+            metal = 89 * self._residence_size
+        else:
+            metal = 1 * self._residence_size
+        plastic = input("Do you recycle plastic?")
+
+        if plastic in positive_responses():
+            plastic = 36 * self._residence_size
+        else:
+            plastic = 1 * self._residence_size
+        glass = input("Do you recycle glass?")
+
+        if glass in positive_responses():
+            glass = 25 * self._residence_size
+        else:
+            glass = 1 * self._residence_size
+        paper = input("Do you recycle paper?")
+
+        if paper in positive_responses():
+            paper = 113 * self._residence_size
+        else:
+            paper = 1 * self._residence_size
+        magazines = input("Do you recycle magazines?")
+
+        if magazines in positive_responses():
+            magazines = 27 * self._residence_size
+        else:
+            magazines = 1 * self._residence_size
+
+        self._recycling_choices = [metal,plastic,glass,paper,magazines]
+
     def calculate_footprint(self):
         """
         calculates the user's carbon footprint
         return: total footprint an integer
         """
-        cumulative_footprint = self._residence_size * 692
+        cumulative_footprint = self._residence_size * 692 - (sum(self._recycling_choices))
         for car in self._cars_list:
-            # Add some logic to handle maintenance. Currently maintenance doesn't come into play at all
-            cumulative_footprint += car.get("lbs_CO2")
+            if car["maintenance"] == True:
+                cumulative_footprint += car.get("lbs_CO2") * .96
+            else:
+                cumulative_footprint += car.get("lbs_CO2") * 1.04
         cumulative_footprint += self._utilities_dict["natural gas"] / 10.68 * (119.58 * 12)
         cumulative_footprint += self._utilities_dict["electricity"] / .1188 * (.92 * 12)
         cumulative_footprint += self._utilities_dict["oil"] / 4.02 * (22.61 * 12)
         cumulative_footprint += self._utilities_dict["propane"] / 2.47 * (12.43 * 12)
 
+        self._footprint = cumulative_footprint
         return int(cumulative_footprint)
+
+    def proactive_choices(self):
+
+        """Actions the user can take to reduce their footprint after being shown
+        what the footprints total amount is. Each input reduces the original footprint"""
+
+
 
 
 def main():
@@ -77,8 +126,13 @@ def main():
     user = User()
     user.get_car_info()
     user.get_utility_info()
+    user.get_recycling_info()
     total_footprint = user.calculate_footprint()
     print(f"Your current carbon footprint per year is {total_footprint} lbs.")
+    #Possibly add in a statement that tells that the above print statement is the max footprint
+    #without carbon abatement practices?
+    print("Here are some actions you can take to reduce your footprint")
+
 
 
 main()
