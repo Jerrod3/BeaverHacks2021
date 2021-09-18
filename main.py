9645fa43e8bb7272edbb0d28849fc8baff31e2a3
Jerrod Lepper <lepperj@oregonstate.edu>
2021-09-18 06:55:06 -0400

    "Added a way to copy the initial user object at a given point, then manipulate it after that point with additional functions to create lists that correspond to different actions after a certain age is met."

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from functions import *
+import copy
 
 
 class User:
@@ -129,18 +130,71 @@ class User:
     def current_lifetime_footprint(self):
 
         """Creates a simple list of accumulated C02 use over the current lifespan of the user. The base for the
-        rest of the lines in the graph up to a given point, the usersa age"""
+        rest of the lines in the graph up to a given point, the users age"""
 
         # Adding in the basic list for the 45 degree "life so far" line
         life_lst = []
         cumulative_CO2 = 0
-        for year in range(18, self._current_age + 1):
+        #Used with a minimum age of 18, since metrics go off of household emissions
+        for year in range(18, self._current_age):
             cumulative_CO2 += self._footprint
             life_lst.append(cumulative_CO2)
 
         self._lifetime_CO2 = life_lst
 
         return sum(life_lst)
+
+    def extrapolate_trend(self):
+
+        """Extrapolating the accumulated C02 over a given set of years, to age 90"""
+
+        life_lst = []
+        #Start with the highest accumulated amount from the user input list
+        cumulative_CO2 = self._lifetime_CO2[-1]
+        for year in range(self._current_age, 90):
+            cumulative_CO2 += self._footprint
+            life_lst.append(cumulative_CO2)
+
+        self._lifetime_CO2 += life_lst
+        return self._lifetime_CO2
+
+    def nuke_the_earth(self):
+
+        """Trendline where the user fails to do anything at all, has 3 extra kids,
+         utility bills are double the amount, and mpg's are half of the users amount, and they
+         drive twice the distance annually"""
+
+        cumulative_footprint = (self._residence_size + 3) * 692
+        for car in self._cars_list:
+            # There is an error here, car.get should be retrieving the key value, but it returns none.
+            print(car.get("annual miles"))
+            cumulative_footprint += (car.get("annual miles") * 2) / (car.get("mpg")/2) * 19.6 * 1.04
+        cumulative_footprint += (self._utilities_dict["natural gas"]*2) / 10.68 * (119.58 * 12)
+        cumulative_footprint += (self._utilities_dict["electricity"]*2) / .1188 * (.92 * 12)
+        cumulative_footprint += (self._utilities_dict["oil"]*2) / 4.02 * (22.61 * 12)
+        cumulative_footprint += (self._utilities_dict["propane"]*2) / 2.47 * (12.43 * 12)
+
+        self._footprint = cumulative_footprint
+
+        return int(cumulative_footprint)
+
+    def goody_two_shoes(self):
+
+        """Trendline where the user does literally everything they can to CO2 emissions(Recycles everything,
+        No vehicle, reduces utilities by half, uses all energy efficient technologies*"""
+
+        # Need to add in electrical mitigations found in the excel sheet
+        cumulative_footprint = self._residence_size * 692 - self._residence_size * 290
+        cumulative_footprint += (self._utilities_dict["natural gas"]/2) / 10.68 * (119.58 * 12)
+        cumulative_footprint += (self._utilities_dict["electricity"]/2) / .1188 * (.92 * 12)
+        cumulative_footprint += (self._utilities_dict["oil"]/2) / 4.02 * (22.61 * 12)
+        cumulative_footprint += (self._utilities_dict["propane"]/2) / 2.47 * (12.43 * 12)
+
+        self._footprint = cumulative_footprint
+
+        return int(cumulative_footprint)
+
+
 
 def main():
     """the main function for creating the user"""
@@ -151,9 +205,18 @@ def main():
     user.get_recycling_info()
     print(f"Your households current carbon footprint per year is {user.calculate_footprint()} lbs.")
     print(f"Your overall lifetime household carbon footprint is {user.current_lifetime_footprint()} lbs.")
-
-
-
-
-
+    # The deep copy function creates an instance of a class object that is identical to the object that is in the
+    # parameter field. I figure we can use this to get copies of the user object's inputs, then modify them through
+    # the functions afterwards. For example, for each line on the graph,
+    # we can modify the "calculate footprint" function to alter the users carbon footprint/year (ie the nuke func)
+    # Then, for every instance, we pass it through the extrapolate trend function afterwards
+    # to project CO2 emissions up to age 90. Test it out below to see what I mean!
+    nuke_user = copy.deepcopy(user)
+    goody = copy.deepcopy(user)
+    nuke_user.nuke_the_earth()
+    goody.goody_two_shoes()
+    print(user.current_lifetime_footprint())
+    print(goody.extrapolate_trend())
+    print(user.extrapolate_trend())
+    print(nuke_user.extrapolate_trend())
 main()
